19a3ec1dbde93d82fc721bc5f84861208cbb477c
cloud-nolojik <cloud@nolojik.com>
2026-05-13 14:19:50 +0530

    fixed edit priduct

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 // config.php - DB connection + session
 session_start();
 
-$DB_HOST = "127.0.0.1";
-$DB_USER = "root";
-$DB_PASS = "";
-$DB_NAME = "crackers_db";
+$DB_HOST = "localhost";
+$DB_USER = "u495609946_admin";
+$DB_PASS = "K8admin@";
+$DB_NAME = "u495609946_crackers_db";
 
 $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if (!$conn) {
